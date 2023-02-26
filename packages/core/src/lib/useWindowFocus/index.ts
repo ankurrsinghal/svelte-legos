bf83bb0eab248528ab77612565fba5c783e594a0
Ankur Singhal <ankurrsinghal@gmail.com>
2023-02-26 17:57:34 +0530

    added useWindowFocus

---
+++ packages/core/src/lib/useWindowFocus/index.ts
@@ -0,0 +1,31 @@
+import { defaultWindow } from '$lib/shared';
+import type { ConfigurableWindow } from '$lib/shared/utils/types';
+import { readable, type Readable } from 'svelte/store';
+
+function isCurrentWindowFocused(window = defaultWindow): boolean {
+	if (!window) return false;
+	if (!window.document) return false;
+	if (typeof window.document.hasFocus !== 'function') return false;
+
+	return window.document.hasFocus();
+}
+
+export function useWindowFocus({ window = defaultWindow }: ConfigurableWindow): Readable<boolean> {
+	const visibility = readable(isCurrentWindowFocused(), (set) => {
+		function handler() {
+			set(isCurrentWindowFocused());
+		}
+
+		if (window) {
+			window.addEventListener('focus', handler);
+			window.addEventListener('blur', handler);
+
+			return () => {
+				window.removeEventListener('focus', handler);
+				window.removeEventListener('blur', handler);
+			};
+		}
+	});
+
+	return visibility;
+}
