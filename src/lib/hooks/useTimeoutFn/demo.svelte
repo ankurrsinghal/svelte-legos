add08718a44c12c5fcc9e142beb3a095b9a3c1c0
Ankur Singhal <ankurrsinghal@gmail.com>
2023-03-05 23:35:49 +0530

    added docs for useTimeoutFn

--- src/lib/hooks/useTimeoutFn/demo.svelte
+++ src/lib/hooks/useTimeoutFn/demo.svelte
@@ -1,1 +1,26 @@
-<h1>useTimeoutFn</h1>
+<script lang="ts">
+import { useTimeoutFn } from '$lib/hooks/useTimeoutFn';
+import DemoContainer from '$lib/shared/components/DemoContainer.svelte';
+import PrimaryButton from '$lib/shared/components/PrimaryButton.svelte';
+
+const defaultText = 'Please wait for 3 seconds';
+
+let text = defaultText;
+
+const { start, isPending } = useTimeoutFn(() => {
+  text = 'Fired!';
+}, 3000);
+
+const restart = () => {
+  text = defaultText;
+  start()
+};
+
+</script>
+  
+<DemoContainer>
+  <p class="mb-4">{text}</p>
+  <PrimaryButton disabled={$isPending} on:click={restart}>
+    Restart
+  </PrimaryButton>
+</DemoContainer>
--- src/lib/shared/components/PrimaryButton.svelte
+++ src/lib/shared/components/PrimaryButton.svelte
@@ -1,1 +1,23 @@
-<button class="text-sm bg-prime text-white px-4 py-2 rounded-md" on:click><slot /></button>
+<script lang="ts">
+  export let disabled: boolean;
+
+  const classNames = `
+    text-sm
+    bg-prime
+    text-white
+    px-4
+    py-2
+    rounded-md
+    disabled:bg-slate-300
+  `;
+</script>
+
+<button
+  disabled={disabled}
+  class={classNames
+    .split("\n")
+    .map((c) => c.trim())
+    .filter(Boolean)
+    .join(" ")}
+  on:click><slot /></button
+>
