b612db7bd97306726a50cfcb9441215241cad627
Syméon Smith <symeon.smith@gmail.com>
2023-03-18 21:43:28 +0100

    feat: highlight current page in navbar

--- src/routes/guides/+layout.svelte
+++ src/routes/guides/+layout.svelte
@@ -1,10 +1,13 @@
 <script lang="ts">
   import type { LayoutData } from "./$types";
+  import { page } from "$app/stores"
   import MenuIcon from "./MenuIcon.svelte";
   export let data: LayoutData;
 
   let isMenuOpened = false;
 
+  $: currentPath = $page.data.hookName;
+  
   $: sidebarClassNames = `
 		flex
 		flex-col
@@ -137,20 +140,22 @@
     <div class="fixed top-0 bottom-0 bg-black right-0" />
     <ul class="space-y-3 lg:mt-8">
       <li>
-        <a href="/guides/" class="hover:underline">Getting Started ({data.actions.length + data.stores.length})</a>
+        <a href="/guides/" class="hover:underline {!currentPath && $page.route.id === "/guides" ? "font-bold" : ""}">
+          Getting Started ({data.actions.length + data.stores.length})
+        </a>
       </li>
       <li class="py-2"></li>
       <li class="text-sm">Actions:</li>
       {#each data.actions as action}
         <li on:click={handleClick} aria-hidden="true">
-          <a href={"/guides/actions/" + action} class="hover:underline">{action}</a>
+          <a href={"/guides/actions/" + action} class="hover:underline {action === currentPath ? "font-bold" : ""}">{action}</a>
         </li>
       {/each}
 			<li class="py-2"></li>
       <li class="text-sm">Stores:</li>
       {#each data.stores as store}
         <li on:click={handleClick} aria-hidden="true">
-          <a href={"/guides/stores/" + store} class="hover:underline">{store}</a>
+          <a href={"/guides/stores/" + store} class="hover:underline {store === currentPath ? "font-bold" : ""}">{store}</a>
         </li>
       {/each}
     </ul>
