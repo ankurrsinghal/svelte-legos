4808a38ed6a0fff5b5b8b0cdd2c9b1304b541c8f
Ankur Singhal <ankurrsinghal@gmail.com>
2023-04-01 02:19:43 +0530

    updated some header information

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,10 +6,21 @@ import { SiteDescription, SiteName, SiteTitle } from "./constants";
 
 <svelte:head>
 	<title>{SiteName} • {SiteTitle}</title>
-
+	<meta charset="UTF-8" />
 	<meta name="twitter:title" content={SiteName} />
 	<meta name="twitter:description" content={SiteTitle} />
 	<meta name="description" content={SiteDescription} />
+
+	<link href="https://sveltelegos.com/" rel="canonical" />
+
+	<meta property="og:title" content={SiteName} />
+	<meta property="og:type" content="website" />
+	<meta property="og:url" content="https://sveltelegos.com" />
+
+	<meta
+		name="keywords"
+		content="sveltejs utilities, sveltejs actions, svelte actions, svelte kit actions, sveltejs stores, sveltejs use, svelte use, svelte, sveltejs, sveltekit, svelte-kit"
+	/>
 </svelte:head>
 
 <div class="bg-gray-50">
