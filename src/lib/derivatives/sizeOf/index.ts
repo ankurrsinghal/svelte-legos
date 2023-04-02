b5048bc104c55012811e28c3f8aa885580db6df5
Ankur Singhal <ankurrsinghal@gmail.com>
2023-04-02 23:36:02 +0530

    added String support to sizeOf as well

--- src/lib/derivatives/sizeOf/index.ts
+++ src/lib/derivatives/sizeOf/index.ts
@@ -2,7 +2,7 @@ import { derived, type Readable } from "svelte/store";
 
 export function sizeOf<T>(store: Readable<T>) {
 	return derived(store, ($store) => {
-		if ($store instanceof Array) {
+		if ($store instanceof Array || typeof $store === "string") {
 			return $store.length;
 		} else if ($store instanceof Set || $store instanceof Map) {
 			return $store.size;
