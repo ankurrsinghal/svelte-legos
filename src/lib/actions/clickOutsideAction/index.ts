aad2ecfa194b515b70cb4fce85ba5363b0aaa2e5
senti <brianmv@uci.edu>
2023-06-04 11:53:05 -0700

    feat: allow passing callback to clickOutside

--- src/lib/actions/clickOutsideAction/index.ts
+++ src/lib/actions/clickOutsideAction/index.ts
@@ -2,13 +2,16 @@ import { listen } from "svelte/internal";
 import type { ActionReturn } from "svelte/action";
 
 interface Attributes {
-	"on:clickoutside": (e: CustomEvent<void>) => void;
+	"on:clickoutside"?: (e: CustomEvent<void>) => void;
 }
 
-export function clickOutsideAction(node: HTMLElement): ActionReturn<{}, Attributes> {
+type Callback = () => unknown
+
+export function clickOutsideAction(node: HTMLElement, callback?: Callback): ActionReturn<{}, Attributes> {
 	const handleClick = (event: Event) => {
 		if (event.target !== null && !node.contains(event.target as Node)) {
 			node.dispatchEvent(new CustomEvent("clickoutside"));
+                        callback?.();
 		}
 	};
 
