301ad4d4b8ab650ee6c49b447a59f85753baaba6
Bobby Mannino <manninobobby@icloud.com>
2023-09-13 16:13:29 +0100

    [feat] added view transitions api utility

---
+++ src/lib/utilities/startViewTransition/demo.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import { startViewTransition } from "$lib";
+
+	startViewTransition()
+</script>
+
+<DemoContainer>
+	<h1>View transitions will not be enabled</h1>
+</DemoContainer>
