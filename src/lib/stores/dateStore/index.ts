bbcf8f339287b6ccba271732c6c17fe5b7599263
Bobby Mannino <manninobobby@icloud.com>
2023-09-15 11:39:59 +0100

    comments

--- src/lib/stores/dateStore/index.ts
+++ src/lib/stores/dateStore/index.ts
@@ -1,5 +1,17 @@
 import { readable } from "svelte/store";
 
+/**
+ * Gets the current date in a Svelte readable store. The date is updated every 10ms.
+ * @see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date
+ *
+ *
+ * @example
+ * ```ts
+ * import { dateStore } from 'svelte-legos';
+ *
+ * $: console.log($dateStore);
+ * ```
+ */
 export const dateStore = readable(new Date(), (set) => {
   const interval = setInterval(() => {
     set(new Date());
