e38aaaf6fae04d3836416354dbc47a97c6364beb
Bobby Mannino <manninobobby@icloud.com>
2023-09-15 11:35:07 +0100

    [comment]

--- src/lib/utilities/startViewTransition/index.ts
+++ src/lib/utilities/startViewTransition/index.ts
@@ -1,5 +1,17 @@
 import { onNavigate } from "$app/navigation";
 
+/**
+ * Starts the view transition API inside SvelteKit.
+ * @see https://developer.mozilla.org/en-US/docs/Web/API/View_Transitions_API
+ *
+ *
+ * @example
+ * ```ts
+ * import { startViewTransition } from 'svelte-legos';
+ *
+ * startViewTransition();
+ * ```
+ */
 export function startViewTransition() {
   onNavigate(({ complete }) => {
     if (!document.startViewTransition) return;
