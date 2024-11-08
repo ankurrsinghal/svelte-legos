2e57dd58ce0c61de1ccdacdbda3a5e0adc52ed1f
Jamie Alavosus <alavosus.james@gmail.com>
2024-11-08 15:12:53 -0500

    windowSizeStore: fix window undefined error

--- src/lib/stores/windowSizeStore/index.ts
+++ src/lib/stores/windowSizeStore/index.ts
@@ -29,7 +29,9 @@ export function windowSizeStore({ window = defaultWindow }: ConfigurableWindow =
 		}
 		
 		return () => {
-			window.removeEventListener("resize", handler);
+			if (window) {
+				window.removeEventListener("resize", handler);
+			}
 		};
 	});
 
